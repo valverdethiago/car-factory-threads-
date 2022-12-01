7d4030080fd53cdab3df5544d69bb0bac16594e8
Thiago Valverde de Souza <valverde.thiago@gmail.com>
2022-12-01 00:36:17 -0300

    Refactor factory to process vehicle assemlys concurrently

--- factory/factory.go
+++ factory/factory.go
@@ -2,6 +2,8 @@ package factory
 
 import (
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"valverdethiago/car-factory-threads/assemblyspot"
 	"valverdethiago/car-factory-threads/vehicle"
 )
@@ -9,13 +11,12 @@ import (
 const assemblySpots int = 5
 
 type Factory struct {
-	vehicleChannel  chan *vehicle.Car
-	AssemblingSpots chan *assemblyspot.AssemblySpot
+	counter int32
 }
 
 func New() *Factory {
 	factory := &Factory{
-		AssemblingSpots: make(chan *assemblyspot.AssemblySpot, assemblySpots),
+		counter: 0,
 	}
 	return factory
 }
@@ -24,21 +25,28 @@ func New() *Factory {
 // (Do not wait for all of them to be assembled to return them all, send each one ready over to main)
 func (f *Factory) StartAssemblingProcess(amountOfVehicles int) {
 	vehicleList := f.generateVehicleLots(amountOfVehicles)
+	wg := &sync.WaitGroup{}
+	wg.Add(amountOfVehicles)
 	vehicleChannel := make(chan *vehicle.Car)
+	readyVehicleChannel := make(chan *vehicle.Car)
 
 	for i := 1; i <= assemblySpots; i++ {
-		go f.worker(i, vehicleChannel)
+		go f.worker(i, vehicleChannel, readyVehicleChannel, wg)
 	}
+
+	go f.processResults(readyVehicleChannel, amountOfVehicles)
 
 	for _, vehicle := range vehicleList {
 		fmt.Println("Sending vehicle to assembly spot ...")
 		vehicleChannel <- vehicle
 	}
+	wg.Wait()
 }
 
-func (Factory) generateVehicleLots(amountOfVehicles int) []*vehicle.Car {
-	var vehicles = []*vehicle.Car{}
-	var index = 0
+func (*Factory) generateVehicleLots(amountOfVehicles int) []*vehicle.Car {
+	var vehicles []*vehicle.Car
+	var index int
+	index = 0
 
 	for {
 		vehicles = append(vehicles, &vehicle.Car{
@@ -111,19 +119,33 @@ func (f *Factory) testCar(car *vehicle.Car) string {
 	return logs
 }
 
-func (f *Factory) worker(id int, vehicleChannel chan *vehicle.Car) {
+func (f *Factory) worker(id int, vehicleChannel chan *vehicle.Car, readyVehicleChannel chan *vehicle.Car, wg *sync.WaitGroup) {
 	for vehicle := range vehicleChannel {
 		fmt.Println("Worker [", id, "] Assembling vehicle...")
 		idleSpot := &assemblyspot.AssemblySpot{}
 		idleSpot.SetVehicle(vehicle)
-		vehicle, err := idleSpot.AssembleVehicle()
+		f.assembleVehicle(id, idleSpot, readyVehicleChannel, wg)
+	}
+}
 
-		if err != nil {
-			fmt.Println("Worker [", id, "] Produced an error while assembling vehicle: ", err.Error())
-			continue
-		}
+func (f *Factory) assembleVehicle(id int, idleSpot *assemblyspot.AssemblySpot, readyVehicleChannel chan *vehicle.Car, wg *sync.WaitGroup) {
+	defer wg.Done()
+	vehicle, err := idleSpot.AssembleVehicle()
 
-		vehicle.TestingLog = f.testCar(vehicle)
-		vehicle.AssembleLog = idleSpot.GetAssembledLogs()
+	if err != nil {
+		fmt.Println("Worker [", id, "] Produced an error while assembling vehicle: ", err.Error())
+	}
+
+	vehicle.TestingLog = f.testCar(vehicle)
+	vehicle.AssembleLog = idleSpot.GetAssembledLogs()
+	readyVehicleChannel <- vehicle
+}
+
+func (f *Factory) processResults(readyVehicleChannel chan *vehicle.Car, amountOfVehicles int) {
+	for ready := range readyVehicleChannel {
+		atomic.AddInt32(&f.counter, 1)
+		fmt.Println("Vehicle ", f.counter, " of ", amountOfVehicles, " assembly process is finished")
+		fmt.Println("\tAssembly logs: ", ready.AssembleLog)
+		fmt.Println("\tTesting logs: ", ready.TestingLog)
 	}
 }
